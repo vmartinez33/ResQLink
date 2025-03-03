63e3fe0538cf4dfb5c96deaa4656847984390e67
Víctor Martínez <108585032+vmartinez33@users.noreply.github.com>
2025-03-03 12:39:24 +0100

    Add backend structure and database config

---
+++ backend/main.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Query
+from sqlmodel import select
+
+from backend.database import create_db_and_tables
+from backend.dependencies import SessionDep
+
+# from backend.models import Hero
+
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+    # clean up code goes here
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+# @app.get("/heroes/")
+# async def read_heroes(
+#     session: SessionDep,
+#     offset: int = 0,
+#     limit: Annotated[int, Query(le=100)] = 100,
+# ) -> list[Hero]:
+#     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+#     return heroes
